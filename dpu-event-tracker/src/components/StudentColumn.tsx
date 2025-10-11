e4bc9cbd382131f01db8841a0f2f0553f2aa9c7d
Redsnapper12 <redmilburn@outlook.com>
2025-10-10 22:13:40 -0400

    Project Setup

---
+++ dpu-event-tracker/src/components/StudentColumn.tsx
@@ -0,0 +1,6 @@
+
+export default function StudentColumn() {
+    return (
+        <div className="grow bg-violet-500 m-3 bg-opacity-20 rounded-xl"></div>
+    )
+}
---
+++ dpu-event-tracker/src/components/UtilityColumn.tsx
@@ -0,0 +1,6 @@
+
+export default function UtilityColumn() {
+    return (
+        <div className="grow bg-violet-500 m-3 bg-opacity-50 rounded-xl"></div>
+    )
+}
